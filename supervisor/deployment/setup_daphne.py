51439325869a3b54c436eb30c7ff3bdd060b4a9b
edjchapman <edchapman88@gmail.com>
2020-09-30 23:03:20 +0100

    Fix directory path in Daphne conf.

--- supervisor/deployment/setup_daphne.py
+++ supervisor/deployment/setup_daphne.py
@@ -30,7 +30,7 @@ class SetupDaphne:
         with open("/etc/supervisor/conf.d/daphne.conf", "w") as conf:
             conf.write("[fcgi-program:asgi]\n")
             conf.write("socket=tcp://0.0.0.0:8000\n")
-            conf.write("directory=/var/www/ws\n")
+            conf.write("directory=/home/supervisor/Supervisor\n")
             conf.write(
                 "command=/home/supervisor/vsuper/bin/daphne -u /run/daphne/daphne%(process_num)d.sock --fd 0 --access-log - --proxy-headers supervisor.asgi:application\n")
             conf.write("numprocs={}\n".format(self.get_numprocs()))
