18150e0ef74a95dd7f0655c8fcfc50293fe208d0
edjchapman <edchapman88@gmail.com>
2020-10-07 21:30:12 +0100

    Add 401 status to conditions to determine if store is offline.

--- stores/services/store_statuses.py
+++ stores/services/store_statuses.py
@@ -3,4 +3,8 @@ import requests
 
 def store_online(store_front):
     r = requests.get(store_front.url)
-    return any(s in r.text.lower() for s in store_front.platform.search_texts())
+    if r.status_code == requests.codes.gone:
+        return False
+    if any(s in r.text.lower() for s in store_front.platform.search_texts()):
+        return False
+    return True
