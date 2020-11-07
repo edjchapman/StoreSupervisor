720b96bbbb8874480125423c484f114e30e3b5ea
ed <edchapman88@gmail.com>
2020-11-07 06:20:29 +0000

    Update supervisor setup scripts.

--- supervisor/deployment/setup.py
+++ supervisor/deployment/setup.py
@@ -15,8 +15,14 @@ with c.cd('/home/supervisor/Supervisor'):
     c.run('git pull origin master')
     c.run('git reset --hard origin/master')
 
-# Setup Daphne
+# Setup Supervisor files
 with c.cd('/home/supervisor/Supervisor'):
     c.run('source ~/.profile; /home/supervisor/vsuper/bin/python supervisor/deployment/supervisor_conf/setup_supervisor_files.py')
 
-# c.sudo('cp /home/supervisor/Supervisor/supervisor/deployment/daphne/daphne.conf /etc/supervisor/conf.d/supervisor1.conf')
+c.sudo('cp /home/supervisor/Supervisor/supervisor/deployment/supervisor_conf/daphne.conf /etc/supervisor/conf.d/daphne.conf')
+c.sudo('cp /home/supervisor/Supervisor/supervisor/deployment/supervisor_conf/celery_beat.conf /etc/supervisor/conf.d/celery_beat.conf')
+c.sudo('cp /home/supervisor/Supervisor/supervisor/deployment/supervisor_conf/celery_worker.conf /etc/supervisor/conf.d/celery_worker.conf')
+
+c.sudo('supervisorctl reread')
+c.sudo('supervisorctl update')
+c.sudo('supervisorctl start all')
--- supervisor/deployment/supervisor_conf/setup_supervisor_files.py
+++ supervisor/deployment/supervisor_conf/setup_supervisor_files.py
@@ -6,7 +6,10 @@ directory = os.path.join(os.path.dirname(__file__))
 for filename in os.listdir(directory):
     if not filename.endswith(".conf"):
         continue
-    with open(os.path.join(directory, filename)) as f:
-        f = f.read().replace("{{env_string}}", env_str)
+    file_path = os.path.join(directory, filename)
+    with open(file_path) as f:
+        f = f.read()
+    with open(file_path) as f_out:
+        f_out.write(f.replace("{{env_string}}", env_str))
     print(f)
     print("*******************************************")
